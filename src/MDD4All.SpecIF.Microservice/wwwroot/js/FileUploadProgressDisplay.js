96e8b80f46edd8ddaec4c6157b61067cb4883c4e
Ed Burke <eb@skoobe.de>
2022-05-27 22:05:16 +0200

    hand edited nuget mismatches

--- src/MDD4All.SpecIF.Microservice/wwwroot/js/FileUploadProgressDisplay.js
+++
@@ -1,9 +0,0 @@
-var FileUploadProgressDisplay = /** @class */ (function () {
-    function FileUploadProgressDisplay() {
-        $("#startUpload").click(function () {
-            console.debug("start upload click");
-            $("#loading").removeAttr("hidden");
-        });
-    }
-    return FileUploadProgressDisplay;
-}());
